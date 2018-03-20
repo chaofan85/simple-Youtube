55789a8e4ace723ae7fe01381d58c4a7e3189b51
Chao Fan <fanchao05@gmail.com>
2018-03-19 21:29:29 -0400

    added search bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import API_KEY from './util/api_key.js';
 
+import API_KEY from './util/api_key.js';
+import SearchBar from './components/search_bar';
 
 const App = () => {
-  console.log(API_KEY);
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
