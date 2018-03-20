eb9ec9737c42bd1b9dc5abdcdf52e828ed6b9bb2
Chao Fan <fanchao05@gmail.com>
2018-03-19 21:22:18 -0400

    added components and api key

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import API_KEY from './util/api_key.js';
+
 
 const App = () => {
+  console.log(API_KEY);
   return <div>Hi!</div>;
 };
 
