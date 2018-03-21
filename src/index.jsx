20bc21f0a358b14eb11c3f0a495814db9bc755dc
Chao Fan <fanchao05@gmail.com>
2018-03-21 17:12:16 -0400

    added YouTube video

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import API_KEY from './util/api_key.js';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 class App extends Component {
   constructor(props) {
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos}/>
       </div>
     );
