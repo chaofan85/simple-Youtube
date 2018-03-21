5f30aedecb80ff38b823ba9f9db66c11429fe8e1
Chao Fan <fanchao05@gmail.com>
2018-03-21 16:39:33 -0400

    added VideoList component

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import API_KEY from './util/api_key.js';
 import SearchBar from './components/search_bar';
-
-
+import VideoList from './components/video_list';
 
 class App extends Component {
   constructor(props) {
@@ -21,6 +20,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
