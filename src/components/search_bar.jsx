46f573ca480530e3c49e396d33ac36f0fc93f0f5
Chao Fan <fanchao05@gmail.com>
2018-03-21 20:32:21 -0400

    updated styling

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -8,9 +8,8 @@ class SearchBar extends Component {
   }
 
   render() {
-    console.log(this.state);
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
           onChange={e => this.setState({ term: e.target.value })}/>
